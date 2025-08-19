d67c1612d1b8248e233858409b54fd9b06aaad4c
VirtualVictoriaa <victoriaa.allison08@gmail.com>
2025-08-19 23:32:56 +0000

    suggestions

---
+++ hostscripts/autoupdate.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#Check for root access.
+#if [ $(id -u) != "0" ]; then
+#        echo "You must be the superuser to run this script."
+#        exit 1
+#fi
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get autoremove -y
+sudo apt-get autoclean
+docker image prune -f
+#docker volume prune -f
+#docker container prune -f
+
+if [ -f /var/run/reboot-required ]
+then
+    echo "$(date +"%F %T") Device updated - Reboot required, restarting in 2 seconds" | sudo tee -a /var/log/auto_update_everything.log
+    ( sleep 2 ; sudo reboot now) &
+else
+    echo "$(date +"%F %T") Device updated - No reboot needed" | sudo tee -a /var/log/auto_update_everything.log
+fi
+exit 0
